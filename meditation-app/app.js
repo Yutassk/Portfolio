21cd91e1c98f38d7af26c7f71b0cb24950762902
Yutassk <sasaki.yt01@gmail.com>
2023-05-31 18:32:51 +0900

    timeDisplayの秒数を二桁表示にしたい。

--- meditation-app/app.js
+++ meditation-app/app.js
@@ -1,1 +1,21 @@
 'use strict';
+const $song = document.querySelector('.song');
+const $play = document.querySelector('#playBtn');
+const $replay = document.querySelector('#replayBtn')
+const $outline = document.querySelector('.moving_outline circle')
+const $video = document.querySelector('.vid_container video');
+
+// Sounds
+const $sounds = document.querySelectorAll('#soundPicker button');
+
+// Time display
+const $timeDisplay = document.querySelector('#timeDipsplay');
+const $outlineLength = outline.getTotalLength();
+
+// Duration
+const $timeSelect =document.querySelectorAll('.time-select button');
+let fakeDuration = 600;
+
+$outline.style.strokeDashoffset = outlineLength;
+$outline.style.strokeDasharray = outlineLength;
+timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(fakeDuration % 60)}`;
