4bed667f86d3d1aa7151eadcdcf0a491e85c3903
Yutassk <sasaki.yt01@gmail.com>
2023-06-06 09:58:01 +0900

    todo coded complete

--- todo-beg/app.js
+++ todo-beg/app.js
@@ -21,16 +21,35 @@ form.addEventListener('submit', function (e) {
 // Todoリストに入力内容を追加
 function add(todo) {
     let todoText = input.value;
-
+    
     if (todo) {
-        todoText = todo;
+        todoText = todo.text;
     }
-
+    
     if (todoText) {  //暗黙的型変換 todoText > 0 がこの一文で可能。
         const li = document.createElement('li');
-
+        
         li.textContent = todoText;
         li.classList.add('list-group-item')
+        
+        // todoに内容があり、completedがついている場合取り消し線反映させる
+        if(todo && todo.completed) {
+            li.classList.add('text-decoration-line-through');
+        }
+
+        // 右クリックで項目削除
+        li.addEventListener('contextmenu', function(e) {
+            e.preventDefault();
+            li.remove();
+            saveData();
+        });
+        
+        // クリックで完了（取り消し線）
+        li.addEventListener('click', function() {
+            li.classList.toggle('text-decoration-line-through');
+            saveData();
+        })
+        
         
         ul.appendChild(li);
         input.value = '';
@@ -42,10 +61,14 @@ function add(todo) {
 function saveData() {
     const lists = document.querySelectorAll('li');
     const todos = [];
-
+    
     lists.forEach((list) => {
-        todos.push(list.textContent);
+        let todo = {
+            text: list.textContent,
+            completed: list.classList.contains('text-decoration-line-through')
+        };
+        todos.push(todo);
     });
-
+    
     localStorage.setItem('todos', JSON.stringify(todos));
 }
