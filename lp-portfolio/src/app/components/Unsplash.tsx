cb9596f96da145a905d33bda436f767a93d579fc
Yutassk <sasaki.yt01@gmail.com>
2023-07-19 16:47:06 +0900

    ✨ unsplashを背景に

--- lp-portfolio/src/app/components/Unsplash.tsx
+++ lp-portfolio/src/app/components/Unsplash.tsx
@@ -8,17 +8,17 @@ export const Unsplash = () => {
 
   useEffect(() => {
     const fetchPhoto = () => {
-      axios.get(`https://api.unsplash.com/photos/random?query=nature&client_id=${process.env.NEXT_PUBLIC_UNSPLASH_API_KEY}`).then((res) => setPhoto(res.data));
+      axios.get(`https://api.unsplash.com/photos/random?query=sea&client_id=${process.env.NEXT_PUBLIC_UNSPLASH_API_KEY}`).then((res) => setPhoto(res.data));
     };
     fetchPhoto();
   }, []);
 
   const getPhotoData = useCallback(() => {
-    axios.get(`https://api.unsplash.com/photos/random?query=nature&client_id=${process.env.NEXT_PUBLIC_UNSPLASH_API_KEY}`).then((res) => setPhoto(res.data));
+    axios.get(`https://api.unsplash.com/photos/random?query=sea&client_id=${process.env.NEXT_PUBLIC_UNSPLASH_API_KEY}`).then((res) => setPhoto(res.data));
   }, []);
 
   return (
-    <div className="h-96 relative">
+    <div className=" fixed top-0 left-0 w-full h-screen -z-10 bg-gray-500 opacity-70">
       {photo && <Result photo={photo} />}
       <SelectButton getPhotoData={getPhotoData} />
     </div>
