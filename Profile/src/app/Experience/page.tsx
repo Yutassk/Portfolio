a819c2574b1e13de2a6aef5b4d29b9aa78801cbe
Yutassk <sasaki.yt01@gmail.com>
2023-08-06 12:25:43 +0900

    🎨 experience CSS調整

--- Profile/src/app/Experience/page.tsx
+++ Profile/src/app/Experience/page.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import MouseStalker from "../components/MouseStalker";
-import { FiArrowUpRight } from "react-icons/fi";
 
 const experience = () => {
   return (
@@ -15,22 +14,15 @@ const experience = () => {
           </div>
 
           <div>
-            <ol className="group/list">
+            <ol>
               <li className="mb-12">
                 <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
                   <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
-                  <header className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2">2023.06 - 現在</header>
+                  <header className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2">2023.04 - 2023.06</header>
                   <div className="z-10 sm:col-span-6">
                     <h3 className="font-medium leading-snug text-slate-200">
-                      <a
-                        className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 group/link text-base"
-                        href="/"
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        <span className="absolute -inset-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
-                        React
-                      </a>
+                      <span className="absolute -inset-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                      React
                     </h3>
                     <p className="mt-2 text-sm leading-normal">
                       コンポーネントですっきりとしたコーディングができるところに魅力を感じ、Reactの学習を開始。
@@ -42,20 +34,11 @@ const experience = () => {
               <li className="mb-12">
                 <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
                   <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
-                  <header className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2">2023.06 - 現在</header>
+                  <header className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2">2023.04 - 2023.06</header>
                   <div className="z-10 sm:col-span-6">
                     <h3 className="font-medium leading-snug text-slate-200">
-                      <div>
-                        <a
-                          className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 group/link text-base"
-                          href="/"
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          <span className="absolute -inset-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
-                          Next.js
-                        </a>
-                      </div>
+                      <span className="absolute -inset-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                      Next.js
                     </h3>
                     <p className="mt-2 text-sm leading-normal">
                       Linkを使用した高速なページ遷移ができたらユーザー体験の向上につながると感じ、学習を開始。
@@ -70,17 +53,8 @@ const experience = () => {
                   <header className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2">2023.06 - 現在</header>
                   <div className="z-10 sm:col-span-6">
                     <h3 className="font-medium leading-snug text-slate-200">
-                      <div>
-                        <a
-                          className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 group/link text-base"
-                          href="/"
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          <span className="absolute -inset-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
-                          TailwindCSS
-                        </a>
-                      </div>
+                      <span className="absolute -inset-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                      TailwindCSS
                     </h3>
                     <p className="mt-2 text-sm leading-normal">
                       Bootstrapのような使いやすさで、より自由にスタイリングができる点に魅力を感じ学習を開始。 直感的にスタイルを当てられるので、とても使いやすい。
@@ -95,17 +69,8 @@ const experience = () => {
                   <header className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2">2023.06 - 現在</header>
                   <div className="z-10 sm:col-span-6">
                     <h3 className="font-medium leading-snug text-slate-200">
-                      <div>
-                        <a
-                          className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 group/link text-base"
-                          href="/"
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          <span className="absolute -inset-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
-                          Paiza（JavaScript）
-                        </a>
-                      </div>
+                      <span className="absolute -inset-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                      Paiza（JavaScript）
                     </h3>
                     <p className="mt-2 text-sm leading-normal">
                       「課題を解決するために必要な考え方をみにつけたい」と思い、Paizaを始めた。Bランクまではスムーズにランクアップできたが、Aランクに上がるためのコーディング力がまだ不足している。Paizaを通して、ポートフォリオなどアプリを作成する際にmap関数やループ文を悩みすぎず使えるようになった。
@@ -119,17 +84,8 @@ const experience = () => {
                   <header className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2">2023.05 - 2023.06</header>
                   <div className="z-10 sm:col-span-6">
                     <h3 className="font-medium leading-snug text-slate-200">
-                      <div>
-                        <a
-                          className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 group/link text-base"
-                          href="/"
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          <span className="absolute -inset-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
-                          BootStrap
-                        </a>
-                      </div>
+                      <span className="absolute -inset-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                      BootStrap
                     </h3>
                     <p className="mt-2 text-sm leading-normal">
                       CSSをベタ書きしていたが、もっと楽をしたいと考えBootStrapを使用した。用意されているパーツを使用する文にはとても快適だったが、アレンジしようとすると少し面倒だったためTailwindCSSに以降した。
@@ -143,17 +99,8 @@ const experience = () => {
                   <header className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2">2023.04 - 2023.06</header>
                   <div className="z-10 sm:col-span-6">
                     <h3 className="font-medium leading-snug text-slate-200">
-                      <div>
-                        <a
-                          className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 group/link text-base"
-                          href="/"
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          <span className="absolute -inset-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
-                          JavaScript
-                        </a>
-                      </div>
+                      <span className="absolute -inset-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                      JavaScript
                     </h3>
                     <p className="mt-2 text-sm leading-normal">
                       静的サイトの実装について学習したので、サイトに動きをつける＆Webアプリ開発に向けてJSの学習を開始した。
@@ -168,17 +115,8 @@ const experience = () => {
                   <header className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2">2023.04 - 2023.05</header>
                   <div className="z-10 sm:col-span-6">
                     <h3 className="font-medium leading-snug text-slate-200">
-                      <div>
-                        <a
-                          className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 group/link text-base"
-                          href="/"
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          <span className="absolute -inset-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
-                          HTML & CSS
-                        </a>
-                      </div>
+                      <span className="absolute -inset-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                      HTML & CSS
                     </h3>
                     <p className="mt-2 text-sm leading-normal">
                       フロントエンドに携わるためにまずはHTML、CSSの基礎知識を習得した。 1ヶ月で模写コーディングを5サイト、実際のページを見ながらコーディングを5サイト作成した。
