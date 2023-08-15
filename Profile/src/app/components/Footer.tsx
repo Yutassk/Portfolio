199b3a69ffbd9382ffccf5a5b6998c16b754cc0b
Yutassk <sasaki.yt01@gmail.com>
2023-08-15 16:39:29 +0900

    🎨 Footer調整

--- Profile/src/app/components/Footer.tsx
+++ Profile/src/app/components/Footer.tsx
@@ -3,7 +3,9 @@ import React from "react";
 export function Footer() {
   return (
     <footer className="max-w-screen-xl pb-16 text-sm text-slate-500 sm:pb-0">
-      <div className="">このサイトはTypeScript、React、Next.js、TailwindCSS、react-springで作成しました。</div>
+      <div className="">
+        このサイトは<span className="text-slate-400"> TypeScript、React、Next.js、TailwindCSS、react-spring </span>で作成しました。
+      </div>
     </footer>
   );
 }
