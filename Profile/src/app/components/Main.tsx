e045f135f756257013cc89b766342f6484bef9ea
Yutassk <sasaki.yt01@gmail.com>
2023-08-15 13:57:24 +0900

    Skillのaタグ削除

--- Profile/src/app/components/Main.tsx
+++ Profile/src/app/components/Main.tsx
@@ -45,23 +45,8 @@ export const Main = () => {
                 <div className="z-10 sm:col-span-6">
                   <h3 className="font-medium leading-snug text-slate-200">
                     <div>
-                      <a
-                        className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 group/link text-base"
-                        href="/"
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        <span className="absolute -inset-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
-                        <span>
-                          React
-                          <div
-                            className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-1"
-                            aria-hidden="true"
-                          >
-                            <FiArrowUpRight />
-                          </div>
-                        </span>
-                      </a>
+                      <span className="absolute -inset-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                      React
                     </div>
                   </h3>
                   <p className="mt-2 text-sm leading-normal">
@@ -78,23 +63,8 @@ export const Main = () => {
                 <div className="z-10 sm:col-span-6">
                   <h3 className="font-medium leading-snug text-slate-200">
                     <div>
-                      <a
-                        className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 group/link text-base"
-                        href="/"
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        <span className="absolute -inset-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
-                        <span>
-                          Next.js
-                          <div
-                            className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-1"
-                            aria-hidden="true"
-                          >
-                            <FiArrowUpRight />
-                          </div>
-                        </span>
-                      </a>
+                      <span className="absolute -inset-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                      Next.js
                     </div>
                   </h3>
                   <p className="mt-2 text-sm leading-normal">
@@ -111,23 +81,8 @@ export const Main = () => {
                 <div className="z-10 sm:col-span-6">
                   <h3 className="font-medium leading-snug text-slate-200">
                     <div>
-                      <a
-                        className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 group/link text-base"
-                        href="/"
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        <span className="absolute -inset-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
-                        <span>
-                          TailwindCSS
-                          <div
-                            className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-1"
-                            aria-hidden="true"
-                          >
-                            <FiArrowUpRight />
-                          </div>
-                        </span>
-                      </a>
+                      <span className="absolute -inset-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                      TailwindCSS
                     </div>
                   </h3>
                   <p className="mt-2 text-sm leading-normal">
@@ -158,7 +113,7 @@ export const Main = () => {
 
       {/* 経歴カード */}
       {/* ここSwitch文使って書き換えられる？ */}
-      <section className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24">
+      <section id="project" className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24">
         <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:p-0 lg:opacity-0">
           <h2 className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">project</h2>
         </div>
