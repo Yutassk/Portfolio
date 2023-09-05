db613485947c98a5ab1ff98ca4aeedee449c3aea
Yutassk <sasaki.yt01@gmail.com>
2023-09-05 09:48:52 +0900

    🎨 Correct missing OPQRSTU for URL & Button design

--- Chousei/src/app/component/GenerateURL.tsx
+++ Chousei/src/app/component/GenerateURL.tsx
@@ -32,7 +32,7 @@ export const GeneratedURL = () => {
   };
 
   const generateRandomString = (length: number) => {
-    const characters = "ABCDDEFGHIJKLMNWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    const characters = "ABCDDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     let randomString = "";
 
     for (let i = 0; i < length; i++) {
@@ -52,20 +52,17 @@ export const GeneratedURL = () => {
     [searchParams]
   );
 
-  const handleUrlGenerate = () => {
+  const handleUrlGenerate = async () => {
     const min = 30;
     const max = 50;
 
     let len = Math.floor(Math.random() * (max - min + 1)) + min;
     const generatedUrl = generateRandomString(len);
 
-    // 生成されたURLをstateに保存
-    // setUrl(generatedUrl);
-    // console.log(url);
     //Selectで選んだ月でシフトを作成
     // try {
-    const docRef = setDoc(doc(db, "shifts", generatedUrl), { シフト月: month });
-    console.log("Document written with ID: ", docRef.then());
+    const docRef = await setDoc(doc(db, "shifts", generatedUrl), { シフト月: month });
+    console.log("Document written with ID: ", docRef);
     // } catch (e) {
     //   console.error("Error adding document: ", e);
     // }
@@ -78,12 +75,12 @@ export const GeneratedURL = () => {
   // }, [searchParams]);
 
   return (
-    <div>
+    <div className="flex flex-col items-center space-y-4 mb-4">
       <div className="flex items-center justify-center">
         <Select className="text-sm " placeholder="シフト月" options={months} onChange={(selectedMonth) => chooseMonth(selectedMonth)} />
         <h3>月の</h3>
       </div>
-      <button className={month ? "bg-pink-300" : "pointer-events-none bg-slate-200"} onClick={handleUrlGenerate}>
+      <button className={`bg-emerald-500 text-white py-2 px-4 rounded-md  ${month ? "hover:bg-emerald-600" : "pointer-events-none opacity-40"}`} onClick={handleUrlGenerate}>
         シフトを作成する
       </button>
     </div>
